0e3308fbf03e215887c25afb4bd12f29d5246955
Elizabeth Lin <68265680+elizabethtl@users.noreply.github.com>
2024-01-11 15:12:09 -0500

    Create worker.py

---
+++ python/rq/worker.py
@@ -0,0 +1,8 @@
+from rq import Queue, Worker
+from redis import Redis
+# Tell RQ what Redis connection to use
+redis_conn = Redis()
+q = Queue('calc_queue', connection=redis_conn)
+
+w = Worker([q], connection=redis_conn)
+w.work()
