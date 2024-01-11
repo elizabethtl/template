1258a767c3159b573995f7081cc2c84b5f6d05d4
Elizabeth Lin <68265680+elizabethtl@users.noreply.github.com>
2024-01-11 15:30:55 -0500

    Update stats.py

--- python/rq/stats.py
+++ python/rq/stats.py
@@ -5,5 +5,15 @@ redis_conn = Redis()
 q = Queue('calc_queue', connection=redis_conn)
 
 print(len(q))
-
 print(q)
+
+
+workers = Worker.all(queue=q)
+print(workers)
+
+w1 = workers[0]
+# w2 = workers[1]
+
+print(f'Successful jobs: {w1.successful_job_count}')
+print(f'Failed jobs: {w1.failed_job_count}')
+print(f'Total working time: {w1.total_working_time}')
